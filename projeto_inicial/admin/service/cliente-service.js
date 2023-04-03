2f72433145eb5611ed02efe90263f0b4a8b69461
Luana <santosmeiraluana@gmail.com>
2023-04-03 16:33:16 -0300

    fetch api

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,22 +1,3 @@
-
-
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-                <td>${email}</td>
-                <td>
-                    <ul class="tabela__botoes-controle">
-                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                    </ul>
-                </td> `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
-
-const tabela = document.querySelector('[data-tabela]')
-
 const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
     .then(resposta => {
@@ -24,10 +5,6 @@ const listaClientes = () => {
     })
 }
 
-listaClientes()
-.then( data => {
-    data.forEach(elemento => {
-    tabela.appendChild(criaNovaLinha(elemento.nome,
-    elemento.email))
-    })
-})
+export const clienteService = {
+    listaClientes
+}
